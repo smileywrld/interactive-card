954e8a24a27e314581f267fbbe9c988dabcd3216
smileywrld <ismaileyyunusa@gmail.com>
2025-11-03 23:44:26 +0100

    Refactor card details to use class selectors for dynamic content

---
+++ script.js
@@ -0,0 +1,7 @@
+let userName = document.querySelector(".username").innerHTML;
+
+let cardNumber = document.querySelector(".card-info").innerHTML;
+
+let cardDate = document.querySelector(".date").innerHTML;
+
+let cardCvc = document.querySelector(".cvc").innerHTML;
