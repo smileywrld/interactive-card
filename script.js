db61ae7791559bbd4ddcfd16d75a1e69938a2bd0
smileywrld <ismaileyyunusa@gmail.com>
2025-11-03 23:52:50 +0100

    Fix input retrieval to use value instead of innerHTML for form fields

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ let cardDate = document.querySelector(".date").innerHTML;
 
 let cardCvc = document.querySelector(".cvc").innerHTML;
 
-let inputName = document.querySelector(".input-name").innerHTML;
+let inputName = document.querySelector(".input-name").value;
+
+let inputDate = document.querySelector(".input-date").value;
+
+let inputCardNum = document.querySelector(".input-card-number").value;
+
+let inputCvc = document.querySelector(".input-cvc").value;
