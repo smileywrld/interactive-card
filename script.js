4f4984754100c1743fa995399a3057ff892405dc
smileywrld <ismaileyyunusa@gmail.com>
2025-11-05 23:17:10 +0100

    Refactor input handling to correctly retrieve values and improve event listener formatting

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
+const submitBtn = document.querySelector(".submit");
+
 const cardElements = {
 	userName: document.querySelector(".username"),
 	cardNumber: document.querySelector(".card-info"),
 	cardDate: document.querySelector(".date"),
-	cardCvc: document.querySelector(".cvc")
+	cardCvc: document.querySelector(".cvc"),
 };
 
 const inputs = {
-	name: document.querySelector(".input-name"),
-	date: document.querySelector(".input-date"),
-	cardNumber: document.querySelector(".input-card-number"),
-	cvc: document.querySelector(".input-cvc")
+	name: document.querySelector(".input-name").value,
+	date: document.querySelector(".input-date").value,
+	cardNumber: document.querySelector(".input-card-number").value,
+	cvc: document.querySelector(".input-cvc").value,
 };
 
 // Update card display when inputs change
-inputs.name.addEventListener('input', () => cardElements.userName.innerHTML = inputs.name.value);
-inputs.cardNumber.addEventListener('input', () => cardElements.cardNumber.innerHTML = inputs.cardNumber.value);
-inputs.date.addEventListener('input', () => cardElements.cardDate.innerHTML = inputs.date.value);
-inputs.cvc.addEventListener('input', () => cardElements.cardCvc.innerHTML = inputs.cvc.value);
+inputs.name.addEventListener(
+	"input",
+	() => (cardElements.userName.innerHTML = inputs.name.value)
+);
+inputs.cardNumber.addEventListener(
+	"input",
+	() => (cardElements.cardNumber.innerHTML = inputs.cardNumber.value)
+);
+inputs.date.addEventListener(
+	"input",
+	() => (cardElements.cardDate.innerHTML = inputs.date.value)
+);
+inputs.cvc.addEventListener(
+	"input",
+	() => (cardElements.cardCvc.innerHTML = inputs.cvc.value)
+);
 
+console.log(inputs.cardNumber);
+
+function confirmDetails() {
+	cardElements.userName = inputs.name;
+}
+
+submitBtn.addEventListener("click", confirmDetails);
